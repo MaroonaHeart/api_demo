e495b86099ba20a76353d43baa74f78df04df8ca
marina_niziy <mary.nizy@yandex.ru>
2025-01-05 23:46:22 +0300

    update crud

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,43 +1,51 @@
 class Api::V1::ProductsController < ApplicationController
   def index
-    products = Product.all
+    @products = Product.all
 
-    render json: products, status: 200
-  end
-
-  def create
-    product = Product.new(
-      name: prod_params[:name],
-      brand: prod_params[:brand],
-      price: prod_params[:price],
-      description: prod_params[:description]
-    )
-
-    if product.save
-      render json: product, status: 200
-    else
-      render json: { error: 'Error creating review!' }
-    end
+    render json: @products
   end
 
   def show
-    product = Product.find_by(id: params[:id])
+    @product = Product.find_by(id: params[:id])
 
-    if product
-      render json: product, status: 200
+    if @product
+      render json: @product
     else
       render json: { error: 'Product not found!' }
     end
   end
 
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      render json: @product, status: :created
+    else
+      render json: { error: 'Error creating review!' }
+    end
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+
+    @product.update(product_params)
+    render json: @product
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+
+    @product.destroy
+  end
+
   private
 
-  def prod_params
-    params.require(:product).permit([
-      :name,
-      :brand,
-      :price,
-      :description
-    ])
+  def product_params
+    params.require(:product).permit(:name, :brand, :price, :description)
   end
 end
+
+#curl -X GET http://localhost:3000/api/v1/products
+#curl -X POST http://localhost:3000/api/v1/products -H "Content-Type: application/json" -d '{"name": "Device", "brand": "Samsung", "price": "$382.00 USD", "description": "New Device"}'
+#curl -X PUT http://localhost:3000/api/v1/products/id -H "Content-Type: application/json" -d '{"name": "Galaxy Buds3 Pro"}'
+#curl -X DELETE http://localhost:3000/api/v1/products/id
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :products, only: %i[index show create]
+      resources :products
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
